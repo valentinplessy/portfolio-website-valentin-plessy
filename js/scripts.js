efe1d0f974e632364172e162cb170b0d9fa228a0
valentinaminus <valplessy@gmail.com>
2021-10-22 20:33:47 +0200

    try to solve js errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,106 +1,106 @@
 // Hide Header on on scroll down
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
-var navbarHeight = $('header').outerHeight();
+var didScroll
+var lastScrollTop = 0
+var delta = 5
+var navbarHeight = $('header').outerHeight()
 
-$(window).scroll(function(event){
-    didScroll = true;
-});
+$(window).scroll(function (event) {
+  didScroll = true
+})
 
-setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
+setInterval(function () {
+  if (didScroll) {
+    hasScrolled()
+    didScroll = false
+  }
+}, 250)
+
+function hasScrolled () {
+  var st = $(this).scrollTop()
+
+  // Make sure they scroll more than delta
+  if (Math.abs(lastScrollTop - st) <= delta)
+    return
+
+  // If they scrolled down and are past the navbar, add class .nav-up.
+  // This is necessary so you never see what is "behind" the navbar.
+  if (st > lastScrollTop && st > navbarHeight) {
+    // Scroll Down
+    $ ('header').removeClass ('nav-down').addClass ('nav-up')
+  } else {
+    // Scroll Up
+    if (st + $(window).height() < $(document).height()) {
+      $('header').removeClass('nav-up').addClass('nav-down')
     }
-}, 250);
+  }
 
-function hasScrolled() {
-    var st = $(this).scrollTop();
-
-    // Make sure they scroll more than delta
-    if(Math.abs(lastScrollTop - st) <= delta)
-        return;
-
-    // If they scrolled down and are past the navbar, add class .nav-up.
-    // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight){
-        // Scroll Down
-        $('header').removeClass('nav-down').addClass('nav-up');
-    } else {
-        // Scroll Up
-        if(st + $(window).height() < $(document).height()) {
-            $('header').removeClass('nav-up').addClass('nav-down');
-        }
-    }
-
-    lastScrollTop = st;
+  lastScrollTop = st;
 }
 
-//-------------- Floating back to top button --------------
-var mybutton = document.getElementById("back-up-button");
-window.onscroll = function() {
+// -------------- Floating back to top button --------------
+var mybutton = document.getElementById('back-up-button');
+window.onscroll = function () {
   scrollFunction()
-};
+}
 
-function scrollFunction() {
+function scrollFunction () {
   if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
-    mybutton.style.display = "block";
+    mybutton.style.display = 'block'
   } else {
-    mybutton.style.display = "none";
+    mybutton.style.display = 'none'
   }
 }
 
-function topFunction() {
-  document.body.scrollTop = 580;
-  document.documentElement.scrollTop = 580;
+function topFunction () {
+  document.body.scrollTop = 580
+  document.documentElement.scrollTop = 580
 }
 
-//-------------- Hamburger Menu --------------
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+// -------------- Hamburger Menu --------------
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-  x.className += ' responsive';
-} else {
-  x.className = 'navtoggle';
-}
+    x.className += ' responsive'
+  } else {
+    x.className = 'navtoggle'
+  }
 }
 
 
-//-------------- Smooth scroll --------------
+// -------------- Smooth scroll --------------
 // Select all links with hashes
 $('a[href*="#"]')
   // Remove links that don't actually link to anything
   .not('[href="#"]')
   .not('[href="#0"]')
-  .click(function(event) {
+  .click(function (event) {
     // On-page links
     if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '')
       &&
-      location.hostname == this.hostname
+      location.hostname === this.hostname
     ) {
       // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      var target = $(this.hash)
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']')
       // Does a scroll target exist?
       if (target.length) {
         // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
+        event.preventDefault()
         $('html, body').animate({
           scrollTop: target.offset().top
-        }, 1000, function() {
+        }, 1000, function () {
           // Callback after animation
           // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
+          var $target = $(target)
+          $target.focus()
+          if ($target.is(':focus')) { // Checking if the target was focused
+            return false
           } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
+            $target.attr('tabindex', '-1') // Adding tabindex for elements not focusable
+            $target.focus() // Set focus again
           };
-        });
+        })
       }
     }
-  });
+  })
